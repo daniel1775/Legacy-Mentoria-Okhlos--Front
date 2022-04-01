f1fa938236810d64d53ca35dbd73baab58c7ca3e
Erika natalia <eri-martin13@hotmail.com>
2022-04-01 09:25:43 -0500

    Mentor whit insert and view function

--- src/views/Administrator/Cruds/CrudMentor/CrudMentor.jsx
+++ src/views/Administrator/Cruds/CrudMentor/CrudMentor.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
 import styles from "./CrudMentor.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEdit, faTrashAlt, faEye } from "@fortawesome/free-solid-svg-icons";
+/* import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; */
+/* import { faEdit, faTrashAlt, faEye } from "@fortawesome/free-solid-svg-icons"; */
 import { makeStyles } from "@material-ui/core/styles";
 import { Modal, TextField } from "@material-ui/core";
 import Axios from "axios";
 import Swal from "sweetalert2";
+import ItemMentor from './components/ItemMentor/ItemMentor'
 
 const baseUrl = process.env.REACT_APP_BACKEND_URL;
 
@@ -135,6 +136,13 @@ const CrudMentor = () => {
   const [modalinsertar, setmodalinsertar] = useState(false);
   const [modaleditar, setmodaleditar] = useState(false);
   const [modalver, setmodalver] = useState(false);
+  //This const saved the values to edit
+  const [choosedData, setChoosedData] = useState({});
+
+  const saveOptionSelected = (data) => {
+    setChoosedData(data);
+  }
+
   //Insert saved module data
   const [SavedData, setSavedData] = useState({
     name: "",
@@ -169,7 +177,7 @@ const CrudMentor = () => {
 
   useEffect(() => {
     Axios({
-      url: `${baseUrl}/all-mentors`,
+      url: `${baseUrl}/mentors`,
     })
       .then((response) => {
         setMentors(response.data);
@@ -203,8 +211,8 @@ const CrudMentor = () => {
   };
 
 
-   //insert mentor fuction
-   async function handleModalInsertMentor() {     
+  //insert mentor fuction
+  async function handleModalInsertMentor() {
     try {
       await Axios.post(`${baseUrl}/mentor`, {
         name: SavedData.name,
@@ -213,6 +221,12 @@ const CrudMentor = () => {
         sons: SavedData.childs,
         num_students: SavedData.assignStu,
         phone: SavedData.phone,
+        email: SavedData.mailMentor,
+        interest: SavedData.interestsMentor,
+        program: SavedData.programMentor,
+        studies: SavedData.studiesMentor,
+        business: SavedData.businessMentor,
+        role: SavedData.roleMentor,
         // 2 habilitado 1 desabilitado
         active: SavedData.state,
         // 1hombre 2mujer 3otro
@@ -831,10 +845,37 @@ const CrudMentor = () => {
           <tbody>
             {mentors.map((e) => {
               return (
-                <tr>
+                <ItemMentor
+                  data={e}
+                  saveOptionSelected={saveOptionSelected}
+                />
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      <Modal open={modalinsertar} onClose={openedClosedModalInsertar}>
+        {bodyInsertar}
+      </Modal>
+
+      <Modal open={modaleditar} onClose={openedClosedModalEditar}>
+        {bodyEditar}
+      </Modal>
+
+      <Modal open={modalver} onClose={openedClosedModalVer}>
+        {bodyVer}
+      </Modal>
+    </div>
+  );
+};
+
+export default CrudMentor;
+
+
+{/* <tr>
                   <td>{e.id}</td>
                   <td>{e.name}</td>
-                  <td> {e.last_name}</td>
+                  <td>{e.last_name}</td>
                   <td>{e.birth_date}</td>
                   <td>{e.gender === 2 ? "Femenino" : e.gender === 1 ? "Masculino" : e.gender === 3 ? "Otro" : null}</td>
                   <td>{e.phone}</td>
@@ -864,25 +905,4 @@ const CrudMentor = () => {
                       </button>
                     </div>
                   </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-      <Modal open={modalinsertar} onClose={openedClosedModalInsertar}>
-        {bodyInsertar}
-      </Modal>
-
-      <Modal open={modaleditar} onClose={openedClosedModalEditar}>
-        {bodyEditar}
-      </Modal>
-
-      <Modal open={modalver} onClose={openedClosedModalVer}>
-        {bodyVer}
-      </Modal>
-    </div>
-  );
-};
-
-export default CrudMentor;
+                </tr> */}
